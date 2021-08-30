680e909a48cf508289f743c61e7ecf790c5cb9a0
Erick Noiztbander <78412093+Noiztbander@users.noreply.github.com>
2021-08-30 14:32:51 +0200

    Three Js Setup

--- src/views/pages/Home/Home.js
+++ src/views/pages/Home/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 import MainHeader from "./../../components/MainHeader";
+import ThreeAnimation from "./../../components/ThreeAnimation";
 
 function Home() {
   return (
     <div className="home">
       <MainHeader />
       <h1>Home page</h1>
+      <ThreeAnimation />
     </div>
   );
 }

522d2cc5776c0f5fd9b9dba2db756d1c74744bae
hugo <hugo.duran.bueno@hotmail.com>
2021-09-01 16:28:13 +0200

    feat: git issues

--- src/views/pages/Profile/Login.js
+++ src/views/pages/Profile/Login.js
@@ -1,17 +1,94 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
 import "./login.css";
 
-function Login() {
-  return (
-    <div className="form-group mt-5">
-      <label for="exampleInputEmail1" class="form-label mt-4">Email address</label>
-      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-      <label for="exampleInputPassword1" class="form-label mt-4">Password</label>
-      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
-      <button type="submit" className="btn btn-dark mt-4">Login</button>
-    </div>
-   
-  );
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: "",
+      loginErrors: "",
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  handleSubmit(e) {
+    const { email, password } = this.state;
+
+    axios
+      .post("/login", {
+        email: email,
+        password: password,
+      })
+      .then((response) => {
+        console.log("response from login", response);
+      })
+      .cath((error) => {
+        console.log("login error", error);
+      });
+
+    e.prevenDefault();
+  }
+
+  render() {
+    return (
+      <div className="form-group mt-5">
+        <form onSubmit={this.handleSubmit}>
+          <label htmlfor="exampleInputEmail1" className="form-label mt-4">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            placeholder="Enter email"
+            value={this.state.email}
+            onChange={this.handleChange}
+            required
+          />
+          <label for="exampleInputPassword1" className="form-label mt-4">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+            placeholder="Password"
+            value={this.state.password}
+            onChange={this.handleChange}
+            required
+          />
+          <button type="submit" className="btn btn-dark mt-4">
+            Login
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
-export default Login;
+// function Login() {
+//   return (
+//     <form>
+//       <div className="form-group mt-5">
+//         <label for="exampleInputEmail1" class="form-label mt-4">Email address</label>
+//         <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+//         <label for="exampleInputPassword1" class="form-label mt-4">Password</label>
+//         <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" required/>
+//         <button type="submit" className="btn btn-dark mt-4">Login</button>
+//       </div>
+//     </form>
+//   );
+// }
+
+// export default Login;
--- src/views/pages/Profile/Signup.js
+++ src/views/pages/Profile/Signup.js
@@ -2,18 +2,53 @@ import React from "react";
 
 function Signup() {
   return (
-    <div className="form-group mt-5">
-      <label for="exampleInputPassword1" class="form-label mt-4">First Name</label>
-      <input type="text" class="form-control" id="exampleInputPassword1" placeholder="First name" />
-      <label for="exampleInputPassword1" class="form-label mt-4">Last Name</label>
-      <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Last name" />
-      <label for="exampleInputEmail1" class="form-label mt-4">Email address</label>
-      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-      <label for="exampleInputPassword1" class="form-label mt-4">Password</label>
-      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
-      <button type="submit" className="btn btn-dark mt-4">Create account</button>
-    </div>
+    <form>
+      <div className="form-group mt-5">
+        <label for="exampleInputPassword1" class="form-label mt-4">
+          First Name
+        </label>
+        <input
+          type="text"
+          class="form-control"
+          id="exampleInputPassword1"
+          placeholder="First name"
+        />
+        <label for="exampleInputPassword1" class="form-label mt-4">
+          Last Name
+        </label>
+        <input
+          type="text"
+          class="form-control"
+          id="exampleInputPassword1"
+          placeholder="Last name"
+        />
+        <label for="exampleInputEmail1" class="form-label mt-4">
+          Email address
+        </label>
+        <input
+          type="email"
+          class="form-control"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+          placeholder="Enter email"
+        />
+        <label for="exampleInputPassword1" class="form-label mt-4">
+          Password
+        </label>
+        <input
+          type="password"
+          class="form-control"
+          id="exampleInputPassword1"
+          placeholder="Password"
+        />
+        <button type="submit" className="btn btn-dark mt-4">
+          Signup
+        </button>
+      </div>
+    </form>
   );
 }
 
 export default Signup;
+
+// select con opcion admin o employee

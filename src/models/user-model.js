bd20bf3acb2cbedf18baf598b66251d871c95782
Erick Noiztbander <78412093+Noiztbander@users.noreply.github.com>
2021-09-03 19:32:41 +0200

    Add to cart with DB Done!

--- src/models/user-model.js
+++ src/models/user-model.js
@@ -11,6 +11,7 @@ const UserSchema = new Schema({
     type: String,
     required: true,
   },
+  cart: { type: Array },
 });
 
 UserSchema.methods.encryptPassword = async (password) => {
--- src/routes/cart-router.js
+++ src/routes/cart-router.js
@@ -4,6 +4,8 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+const userModel = require("../models/user-model");
+const productModel = require("../models/product-model");
 const { verifyAuthentication } = require("../helpers/auth");
 
 // read DB
@@ -17,6 +19,16 @@ router.get("/", verifyAuthentication, async (req, res) => {
       res.status(500).send(err);
     }
   });
+});
+
+// read Data from DB
+router.post("/add-product/:id", verifyAuthentication, async (req, res) => {
+  const foundProduct = await productModel.findById(req.params.id);
+  console.log(foundProduct);
+  await userModel.findByIdAndUpdate(req.user._id, {
+    $push: { cart: foundProduct },
+  });
+  res.redirect("/products");
 });
 
 module.exports = router;

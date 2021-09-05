99063bb5149a1db9ea6b43b8bdb224a57ba9da9a
hugo <hugo.duran.bueno@hotmail.com>
2021-09-05 19:03:35 +0200

    credit card layout

--- src/views/pages/Checkout/Checkout.js
+++ src/views/pages/Checkout/Checkout.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 import CreditCard from "../../components/CreditCard/CreditCard";
 
@@ -9,56 +9,80 @@ function Checkout() {
   const [cardExpirationDateAnimation, setCardExpirationDateAnimation] =
     useState("");
 
-  // useEffect(() => {
-  //   cardNameAnimation("Ethan");
-  // }, []);
-
   return (
-    <div className="container-fluid d-flex flex-row justify-content-center align-items-center w-100 mt-3 gap-3">
+    <div className="container-fluid d-flex flex-row justify-content-center align-items-center w-100 mt-5 gap-3">
       <form className="w-100">
         <div className="form-group">
-          <label htmlFor="exampleInputEmail1" className="form-label mt-4">
-            CardName
+          <label htmlFor="exampleInputName" className="form-label mt-4">
+            Card Name
           </label>
           <input
-            type="email"
+            type="text"
+            name="text"
             className="form-control"
-            id="exampleInputEmail1"
+            id="exampleInputName"
             onChange={(event) => {
               return setCardNameAnimation(event.target.value);
             }}
             aria-describedby="emailHelp"
-            placeholder="Enter Your name"
+            placeholder="Enter your name"
             value={cardNameAnimation}
+            maxLength="50"
           />
-          <small id="emailHelp" className="form-text text-muted">
-            We'll never share your email with anyone else.
-          </small>
         </div>
         <div className="form-group">
-          <label for="exampleInputPassword1" className="form-label mt-4">
-            Password
+          <label htmlFor="exampleInputCardNumber" className="form-label mt-4">
+            Card Number
+          </label>
+          <input
+            type="text"
+            name="text"
+            className="form-control"
+            id="exampleInputCardNumber"
+            onChange={(event) => {
+              return setCardNumberAnimation(event.target.value);
+            }}
+            placeholder="Enter your card number"
+            value={cardNumberAnimation}
+            maxLength="16"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="exampleExpiryNumber" className="form-label mt-4">
+            Expiry Date
           </label>
           <input
             type="password"
             className="form-control"
-            id="exampleInputPassword1"
-            placeholder="Password"
+            id="exampleExpiryNumber"
+            onChange={(event) => {
+              return setCardExpirationDateAnimation(event.target.value);
+            }}
+            placeholder="mm/yy"
+            value={cardExpirationDateAnimation}
+            maxLength="5"
           />
         </div>
         <div className="form-group">
-          <label for="exampleSelect1" className="form-label mt-4">
-            Example select
+          <label htmlFor="exampleCvvCardNumber" className="form-label mt-4">
+            CVV
           </label>
-          <select className="form-select" id="exampleSelect1">
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
-          </select>
+          <input
+            type="number"
+            name="number"
+            className="form-control"
+            id="exampleCvvCardNumber"
+            onChange={(event) => {
+              return setCardCVVCodeAnimation(event.target.value);
+            }}
+            placeholder="Enter your cvv"
+            value={cardCVVCodeAnimation}
+            maxLength="3"
+          />
+          <button className="btn btn-dark mt-5">Finish purchase</button>
         </div>
       </form>
+
       <div className="h-75 w-100 pe-3">
         <div>
           <CreditCard
